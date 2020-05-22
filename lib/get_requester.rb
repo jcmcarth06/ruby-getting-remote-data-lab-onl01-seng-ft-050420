0f41e6fc136b33d3b304c05c26809ca4da189a12
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-22 20:05:37 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,15 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+
+class GetRequester
+
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+  def get_response
+  
+  uri = URI.parse(URL)
+     response = Net::HTTP.get_response(uri)
+     response.body
+
+    
+end
